b9991a9b6c1e5fe56f14d9672cc864f427ba0ed7
shumerex <163420582+shumerex@users.noreply.github.com>
2024-03-20 23:49:24 +0700

    Add files via upload

--- homework6.py
+++ homework6.py
@@ -1,6 +1,6 @@
-list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 cars_count = 0
-for i in list_:
+for i in car:
     print('Я езжу на автомабиле марки', i)
     cars_count += 10
     print(cars_count)
